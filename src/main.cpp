f5f91b75cb9f1552f73d0ecb26ec53f0337fdebd
Ronald <ronald.drp11@gmail.com>
2019-04-14 12:04:28 -0300

    updated code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "libs/useritem.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::array<UserItem, 10> ui;
+    std::vector<std::shared_ptr<UserItem>> ui;
+    ui.push_back(std::make_shared<UserItem>());
     std::cout << "test" << std::endl;
+    
     return 0;
 }
