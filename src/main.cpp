4cf949eab44fb4f3097c92fe3409b97c2b49f211
Ronald Pereira <ronaldpereira@users.noreply.github.com>
2019-04-14 11:37:35 -0300

    removed namespace std to avoid conflicts

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "libs/useritem.hpp"
 
-using namespace std;
-
 int main(int argc, char const *argv[])
 {
     UserItem ui[10];
-    cout << "test" << endl;
+    std::cout << "test" << std::endl;
     return 0;
 }
