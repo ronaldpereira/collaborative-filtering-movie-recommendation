b8334d1113d6b27e0f1e9e8898bede8333952200
Ronald <ronald.drp11@gmail.com>
2019-04-19 12:41:00 -0300

    matrix building ready

--- src/libs/useritem.cpp
+++ src/libs/useritem.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 #include <cstring>
 #include <string>
-#include <string.h>
-#include <stdlib.h>
 #include "useritem.hpp"
 
 void UserItem::MatrixBuilder(char *ratingsPath) {
@@ -14,7 +12,7 @@ void UserItem::MatrixBuilder(char *ratingsPath) {
     // Discard header from csv
     getline(ratingsFile, line);
 
-    while(getline(ratingsFile, line) && !line.empty()) {
+    while (getline(ratingsFile, line) && !line.empty()) {
         char *token;
 
         char *cstr = new char [line.length()+1];
@@ -32,6 +30,74 @@ void UserItem::MatrixBuilder(char *ratingsPath) {
         token = strtok(NULL, ",ui");
         int timestamp = atoi(token);
 
-        std::cout << user << " " << item << " " << rating << " " << timestamp << std::endl;
+        insertUserItemRating(user, item, rating);
     }
+
+    std::cout << "n_users: " << matrix.size() << std::endl;
+    std::cout << "n_items: " << matrix[0].size() << std::endl;
+}
+
+void UserItem::insertUserItemRating(int userID, int itemID, int rating) {
+    int userXPos = getUserPosition(userID);
+    int itemYPos = getItemPosition(itemID);
+
+    // User is not inserted
+    if (userXPos < 0) {
+        userXPos = createUser(userID);
+    }
+
+    // Item is not inserted
+    if (itemYPos < 0) {
+        itemYPos = createItem(itemID);
+    }
+
+    matrix[userXPos][itemYPos] = rating;
+}
+
+int UserItem::getUserPosition(int userID) {
+    for (unsigned int i = 0; i < userLookup.size(); i++)
+    {
+        // If the userID is found in the userLookup, returns the index that corresponds to useritem matrix coordenate X
+        if (userID == userLookup[i])
+            return i;
+    }
+    // If it's not found, return -1
+    return -1;
+}
+
+int UserItem::getItemPosition(int itemID) {
+    for (unsigned int i = 0; i < itemLookup.size(); i++)
+    {
+        // If the itemID is found in the itemLookup, returns the index that corresponds to useritem matrix coordenate Y
+        if (itemID == itemLookup[i])
+            return i;
+    }
+    // If it's not found, return -1
+    return -1;
+}
+
+int UserItem::createUser(int userID) {
+    nUser++;
+
+    // Insert user to matrix
+    matrix.push_back(std::vector<int> (nItem));
+
+    // Insert user to userLookup
+    userLookup.push_back(userID);
+
+    return nUser-1;
+}
+
+int UserItem::createItem(int itemID) {
+    nItem++;
+
+    // Insert item to matrix
+    for (int i = 0; i < nUser; i++) {
+        matrix[i].push_back(0);
+    }
+
+    // Insert item to itemLookup
+    itemLookup.push_back(itemID);
+
+    return nItem-1;
 }
--- src/libs/useritem.hpp
+++ src/libs/useritem.hpp
@@ -1,14 +1,23 @@
 #ifndef USERITEM
 #define USERITEM
 #include <vector>
-#include <array>
 
 class UserItem
 {
 private:
+    // Data
     std::vector<std::vector<int>> matrix;
-    std::vector<int> userPosition, itemPosition;
-    
+    std::vector<int> userLookup;
+    std::vector<int> itemLookup;
+    int nUser = 0;
+    int nItem = 0;
+
+    // Methods
+    void insertUserItemRating(int, int, int);
+    int getUserPosition(int);
+    int getItemPosition(int);
+    int createUser(int);
+    int createItem(int);
 public:
     void MatrixBuilder(char*);
 };
